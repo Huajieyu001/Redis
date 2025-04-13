63c3aae1af05bfa11a312b6732bc9a537b5981a1
Huajieyu001 <331173500@qq.com>
2025-04-13 09:41:49 +0800

    0413B

--- redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
+++ redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
@@ -1,7 +1,10 @@
-package com.heima;
+package cn.itcast.redisdemo;
 
+import io.lettuce.core.ReadFrom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisDemoApplication {
